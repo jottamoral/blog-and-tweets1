5e4da148b769f45fffb1cd5d5b695b4d725089dd
Jose Manuel Morales <moralesjmd2002@gmail.com>
2025-04-02 13:15:25 -0500

    Ejercicio hasta comienzo litar entries

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/entries/create', [App\Http\Controllers\EntryController::class, 'create']);
+
+Route::post('/entries/create', [App\Http\Controllers\EntryController::class, 'store']);
